f7114dd01d5910a4efafaa847c747246cdaf767a
Sejal <sejaldampana@gmail.com>
2025-10-30 21:57:31 +0530

    Create main.py

---
+++ Perfomance_Analysis/src/main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO("yolo11n.pt")
+# Train the model using the 'coco8.yaml' dataset for 'n' epochs
+results = model.train(data="coco8.yaml", epochs=3)
+# Evaluate the model's performance on the validation set
+results = model.val()
+# Export the model to ONNX format
+success = model.export(format="onnx")
